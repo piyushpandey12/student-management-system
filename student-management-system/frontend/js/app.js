c136c44a46960a175fd9302684e62234d236b2f6
Piyush Pandey <piyushpandey.ssm95@gmail.com>
2026-04-04 16:44:16 +0530

    Fix frontend API URL

--- student-management-system/frontend/js/app.js
+++ student-management-system/frontend/js/app.js
@@ -3,7 +3,7 @@ const BASE_URL =
     window.location.hostname === "127.0.0.1" ||
     window.location.hostname === "localhost"
         ? "http://127.0.0.1:5000"
-        : "https://student-management-system.onrender.com";
+        : "https://student-management-system-jg5j.onrender.com";
 
 
 // ================= COMMON FETCH =================
