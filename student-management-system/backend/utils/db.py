d096c6341f1f65434e73b48b5aac21b4187310cb
Piyush Pandey <piyushpandey.ssm95@gmail.com>
2026-04-06 17:22:06 +0530

    final auth fix

--- student-management-system/backend/utils/db.py
+++ student-management-system/backend/utils/db.py
@@ -1,28 +1,24 @@
 import mysql.connector
-from config import DB_CONFIG
 
 
 def get_connection():
     try:
         conn = mysql.connector.connect(
-            host=DB_CONFIG["host"],
-            user=DB_CONFIG["user"],
-            password=DB_CONFIG["password"],
-            database=DB_CONFIG["database"],
-            port=DB_CONFIG["port"],
-            connection_timeout=10
+            host="centerbeam.proxy.rlwy.net",   
+            user="root",                       
+            password="gpNYiPlqWaeXxYcsqssdwgSeDPWshwR",  
+            database="railway",               
+            port=55552                         
         )
 
-        if conn.is_connected():
-            return conn
-
-        return None
+        print("✅ DB Connected Successfully")
+        return conn
 
     except Exception as e:
         print("❌ DB CONNECTION ERROR:", e)
         return None
 
 
-# Backward compatibility
+# Optional (for compatibility)
 def get_db_connection():
     return get_connection()
